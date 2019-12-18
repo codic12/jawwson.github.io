c17e7dcb1a23d0b20c03b4a8828eaa624307f1b6
Jason is Jawwson <59033313+jawwson@users.noreply.github.com>
2019-12-18 15:01:08 -0800

    Does this work?
    Don't lie

---
+++ DoesThisWork.py
@@ -0,0 +1,7 @@
+print("Hi, does this work?")
+isyes = input(" ")
+lower(isyes)
+if isyes == "yes":
+  print('yay it works!')
+else:
+  print('Well... you either typed extra spaces or didn't type yes. LIAR THIS WORKS!!!')
