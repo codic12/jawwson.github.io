f61b7b78822f3527e68daca54a0935ee37274d93
Gurjus Bhasin <48339289+gsbhasin123@users.noreply.github.com>
2019-12-18 19:57:42 -0800

    CODE REFACTOR
    So remember to use the strip() and lower() methods; strip() removes whitespace!
    I also changed it to check if yes is in the string, not if the string is equal to yes; In the future i might build a simple AI lib for this, lol. not real ai, just scanning a huge list.

--- DoesThisWork.py
+++ DoesThisWork.py
@@ -1,7 +1,6 @@
-print("Hi, does this work?")
-isyes = input(" ")
-lower(isyes)
-if isyes == "yes":
+isyes = input("Hi, does this work? ")
+isyes = isyes.lower().strip()
+if "yes" in isyes:
   print('yay it works!')
 else:
-  print('Well... you either typed extra spaces or didn't type yes. LIAR THIS WORKS!!!')
+  print("Well... you either typed extra spaces or didn't type yes. LIAR THIS WORKS!!!")
